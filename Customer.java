0ca51c91bc9dc4b6344d2df8c2ac2180e52390a4
royhsle0120 <royhslee0120@gmail.com>
2025-12-17 21:04:53 +0900

    Customer 클래스 생성하기

--- Customer.java
+++ Customer.java
@@ -1,4 +1,32 @@
 package commerce;
 
 public class Customer {
+
+    private String name;
+    private String email;
+    private String grade;
+
+    public Customer(String name, String email, String grade) {
+        this.name = name;
+        this.email = email;
+        this.grade = grade;
+    }
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+        @Override
+    public String toString() {
+        return String.format("고객명: %s | 이메일: %s | 등급: %s",name, email, grade);
+    }
 }
