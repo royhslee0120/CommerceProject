0aea3d6fca3497fa6c523a70349db50e6641e738
royhsle0120 <royhslee0120@gmail.com>
2025-12-18 20:48:57 +0900

    step4 캡슐화하기

--- Product.java
+++ Product.java
@@ -2,10 +2,10 @@ package commerce;
 
 public class Product {
 
-    String name;
-    int price;
-    String info;
-    int amount;
+    private String name;
+    private int price;
+    private String info;
+    private int amount;
 
     public Product(String name, int price, String info, int amount) {
         this.name = name;
@@ -13,12 +13,18 @@ public class Product {
         this.info = info;
         this.amount = amount;
     }
+
+    public String getName() { return name; }
+    public int getPrice() { return price; }
+    public String getInfo() { return info; }
+    public int getAmount() { return amount; }
+
     @Override
     public String toString() {
         return String.format("%-11s | %,10d원 | %s", name, price, info);
     }
 
-    public String toDetailSting() {
+    public String toDetailString() { // 오타 수정
         return String.format("%-11s | %,10d원 | %s | 재고: %d개", name, price, info, amount);
     }
 
